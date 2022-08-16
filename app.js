f430b056ef197a5e57a11aae82da525ea6269aac
victorvictoria-maker <viktohrier01@gmail.com>
2022-08-16 16:17:14 +0100

    first commit just after intiating app with express and connection to the database

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDatabase = require('./config/db'); 
+const morgan = require('morgan');
+
+// Load ENV Config
+dotenv.config({path: './config/config.env'});
+
+// connect to database
+connectDatabase();
+
+const app = express();
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+};
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    );
+
+
---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            // useFindAndModify: false
+            retryWrites: true,
+            w: "majority",
+        });
+
+        console.log(`MONGODB Connected: ${conn.connection.host}`);
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    };
+};
+
+module.exports = connectDB;
