7d131c5ad7f8c777ee4336d26d0be9029f86058c
victorvictoria-maker <viktohrier01@gmail.com>
2022-08-17 12:34:30 +0100

    Authentication done, working perfectly well

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
+const User = require('../models/User');
 const UserModel = require('../models/User');
 
 
@@ -14,18 +15,51 @@ module.exports = function(passport) {
         // User.findOrCreate({ googleId: profile.id }, function (err, user) {
         // return cb(err, user);
         // });
-            console.log(profile);
+            // console.log(profile);
+            const newUser= {
+                googleId: profile.id,
+                displayName: profile.displayName,
+                firstName: profile.name.givenName,
+                lastName: profile.name.familyName,
+                image: profile.photos[0].value
+            };
+
+            try {
+                let user = await User.findOne({googleId: profile.id});
+
+                if(user) {
+                    done(null, user);
+                } else {
+                    user = await User.create(newUser);
+                    done(null, user);
+                }
+
+            } catch (err) {
+                console.log(err);
+            };
         }
     ));
 
 
-    passport.serializeUser((user, done) => {
-        done(null, user.id);
-    });
+    // passport.serializeUser((user, done) => {
+    //     done(null, user.id);
+    // });
 
-    passport.deserializeUser((id, done) => {
-        User.findByid(id, (err, user) => {
-            done(err, user);
+    // passport.deserializeUser((id, done) => {
+    //     User.findByid(id, (err, user) => {
+    //         done(err, user);
+    //     });
+    // });
+
+    passport.serializeUser(function(user, cb) {
+        process.nextTick(function() {
+          cb(null, { id: user.id, username: user.username });
         });
-    });
+      });
+      
+      passport.deserializeUser(function(user, cb) {
+        process.nextTick(function() {
+          return cb(null, user);
+        });
+      });
 };
